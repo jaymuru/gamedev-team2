dea69a567dd53ea74d1014943a79a2168e9bbe5f
Ariel-Amoguis <arielamoguis7@gmail.com>
2026-05-10 19:42:22 +0800

    Update gametask.py

--- gametask.py
+++ gametask.py
@@ -2,13 +2,13 @@ from random import choice
 
 #monster/enemy
 monsters = [
-    ("Zephyr", 13, 30),
-    ("Pyro", 12, 29),
-    ("Terra", 11, 28),
-    ("Hydra", 10, 27),
+    ("Zephyr", 13, 30, 30),
+    ("Pyro", 12, 29, 29),
+    ("Terra", 11, 28, 28),
+    ("Hydra", 10, 27,27),
 ]
 
-room_dungeon = 0
+dungeon_room = 1
 
 print("Welcome to the dungeon game!")
 #player 
@@ -48,28 +48,20 @@ def attack():
     print(f"{monstname} HP is now {monsthp}.")
     gain_experience(20)
 
-    if monsthp > 0:
-        monster_attack()
-        print(f"\nThe monster dealt {monstatk} damage to you!")  
-        print(f"Player HP: {player_hp}\n")
-
 def block():
     print("\nYou defend against the attack!")
     gain_experience(10)
 
 def heal():
     global player_hp
-    if player_hp < 65:
-        player_hp += 8
-        if player_hp >= 65:
-            player_hp = 65
+    if player_hp < 80:
+        player_hp += 13
+        if player_hp >= 80:
+            player_hp = 80
         print(f"Player HP: {player_hp}")
     else:
         print("HP is already full!")
     gain_experience(10)
-
-    if monsthp > 0:
-        monster_attack()
 
 def counter_attack(): #nagtake ng damage yung player pero mas mababa sa original damage ng monster since nag counter attack player
     counterdamge = 6
@@ -78,10 +70,6 @@ def counter_attack(): #nagtake ng damage yung player pero mas mababa sa original
     monsthp -= counterdamge
     print(f"\nYou've countered an attack and dealt {counterdamge} damage! ")
     print(f"{monstname} HP is now {monsthp}.")
-    if monsthp > 0:
-        monster_attack()
-        print(f"\nThe monster dealt {monstatk} damage to you!")  
-        print(f"Player HP: {player_hp}\n")
     gain_experience(10)
 
 def display_stats():
@@ -110,32 +98,27 @@ def display_mainmenu():
     for menu_item in mainmenu:
         print(menu_item)
 
-def display_menu2():
-    menu2 = ["1: Continue fighting", "2: View Stats", "3: Return to main menu"]
-    print("\nWhat do you want to do next?")
-    for menu_item in menu2:
-        print(menu_item)
-
 def monster_attack():
     global player_hp
     player_hp -= monstatk
+    print(f"\nThe monster dealt {monstatk} damage to you!\n")  
+    print(f"HP: {player_hp}\n")
 
 def player_attack():
-    global class_choice, monsthp, gold
-
+    global class_choice
     if class_choice == "Fighter" or class_choice == "fighter":
-            fighter_attack = input("\nChoose an action (attack, block, retreat): \n> ")
-            if fighter_attack == "attack":
-                attack()             
-            
-            elif fighter_attack == "block":
-                block()
-            
-            elif fighter_attack == "retreat":
-                print("You run away! Returning to main menu.")
-                return True
-            else:
-                print("Invalid action, try again.")
+        fighter_attack = input("\nChoose an action (attack, block, retreat): \n> ")
+        if fighter_attack == "attack":
+            attack()             
+        
+        elif fighter_attack == "block":
+            block()
+        
+        elif fighter_attack == "retreat":
+            print("You run away! Returning to main menu.")
+
+        else:
+            print("Invalid action, try again.")
 
     elif class_choice == "Mage" or class_choice == "mage":
         mage_attack = input("\nChoose an action (attack, heal, retreat): \n> ")
@@ -147,8 +130,7 @@ def player_attack():
             
         elif mage_attack == "retreat":
             print("You run away! Returning to main menu.")
-            return True 
-            
+
         else:
             print("Invalid action, try again.")
 
@@ -162,36 +144,36 @@ def player_attack():
             
         elif assassin_attack == "retreat":
             print("You run away! Returning to main menu.")
-            return True
+
         else:
             print("Invalid action, try again.")
+
+    # if monsthp <= 0:
+    #     gold += 10
+    #     print(f"\nYou defeated the {monstname}!\n")
+    #     print(f"\nYou gain 10 golds for defeating the monster!\n")
+    #     return True
     
-    if monsthp <= 0:
-        gold += 10
-        print(f"\nYou defeated the {monstname}!\n")
-        print(f"\nYou gain 10 golds for defeating the monster!\n")
-        return True
-    
-    return False 
+    # return False 
 
 while True:
     if class_choice == "Fighter" or class_choice == "fighter":
         print("You have selected fighter class! ")
-        permanent_playerhp = 75
-        player_hp = 75
+        permanent_playerhp = 100
+        player_hp = 100
         player_atk = 10
         break
     elif class_choice == "Mage" or class_choice == "mage":
         print("You have selected mage class! ")
-        permanent_playerhp = 65
-        player_hp = 65
-        player_atk = 6
+        permanent_playerhp = 80
+        player_hp = 80
+        player_atk = 8
         break
     elif class_choice == "Assassin" or class_choice == "assassin":
         print("You have selected assassin class! ")
-        permanent_playerhp = 70
-        player_hp = 70
-        player_atk = 8
+        permanent_playerhp = 90
+        player_hp = 90
+        player_atk = 9
         break
     else:
         print("Invalid Class Choice!")
@@ -202,17 +184,86 @@ while True:
     action = input("> ")
 
     if action == "1":
-        print("\nYou entered a dungeon.")
-        monstname, monstatk, monsthp = spawn_monster(monsters)
+        print(f"\nYou entered a dungeon. Room {dungeon_room}")
+        monstname, monstatk, monsthp, monster_hp = spawn_monster(monsters)
         print(f"You have encountered a {monstname} wtih {monsthp} HP! ")
 
-        while monsthp > 0:
-            defeated = player_attack()
-            if defeated:
+        while True:
+            if class_choice == "Fighter" or class_choice == "fighter":
+                fighter_attack = input("\nChoose an action (attack, block, retreat): \n> ")
+                if fighter_attack == "attack":
+                    attack()             
+                
+                elif fighter_attack == "block":
+                    block()
+                
+                elif fighter_attack == "retreat":
+                    print("You run away! Returning to main menu.")
+                    break
+                else:
+                    print("Invalid action, try again.")
+                    continue
+
+            elif class_choice == "Mage" or class_choice == "mage":
+                mage_attack = input("\nChoose an action (attack, heal, retreat): \n> ")
+                if mage_attack == "attack":
+                    attack()             
+                    
+                elif mage_attack == "heal":
+                    heal()
+                    
+                elif mage_attack == "retreat":
+                    print("You run away! Returning to main menu.")
+                    break
+                else:
+                    print("Invalid action, try again.")
+                    continue
+
+            elif class_choice == "Assassin" or class_choice == "assassin": 
+                assassin_attack = input("\nChoose an action (attack, counter, retreat): \n> ")
+                if assassin_attack == "attack":
+                    attack()             
+                    
+                elif assassin_attack == "counter":
+                    counter_attack()
+                    
+                elif assassin_attack == "retreat":
+                    print("You run away! Returning to main menu.")
+                    break
+                else:
+                    print("Invalid action, try again.")
+                    continue
+            if monsthp > 0:
+                monster_attack()
+            else:
+                gold += 10
+                print(f"\nYou defeated the {monstname}")
+                print(f"\nYou gain 10 golds for defeating the monster!\n")
+                monsthp = monster_hp
+                dungeon_room += 1
+                if dungeon_room <= 3:
+                    print(f"\nYou are in room {dungeon_room} of the dungeon! ")
+                    monstname, monstatk, monsthp, monster_hp = spawn_monster(monsters)
+                    print(f"New {monstname} with {monsthp} appeared! ")
+                    continue
+                else:
+                    dungeon_room = 1
+                    print("\nYou cleared the dungeon! Returning to main menu")
+                    break
+            if player_hp > 0:
+                continue
+            else:
+                print("You died! Returning to main menu")
+                player_hp = permanent_playerhp
                 break
+
+        # while monsthp > 0:
+        #     defeated = player_attack()
+        #     if defeated:
+        #         break
     
-            if monsthp <= 0:
-                spawn_monster(monsters)
+        #     if monsthp <= 0:
+        #         spawn_monster(monsters)
         
     elif action == "2":
         display_stats()
@@ -225,14 +276,3 @@ while True:
     else: 
         print("Invalid Choice! Enter number between 1 to 3!")
         continue
-    display_menu2()
-    next_choice = input("> ")
-    if next_choice == "1":
-        continue # continue is used to skip the rest of the code in the loop and start the next iteration of the loop(continue fighting/the loop)
-    elif next_choice == "2":
-        current_stats() 
-    elif next_choice == "3":
-        player_hp = permanent_playerhp
-        print("Returning to the main menu.\n")
-    else:
-        print("Invalid choice. Returning to main menu!\n")
